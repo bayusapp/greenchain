67df0d814b3e660922221da937521f86afc62525
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2024-12-05 22:21:45 +0700

    tambah routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::index');
+
+$routes->get('/Beranda', 'Beranda::index');
+
+$routes->get('petani/(:num)', 'EthereumController::viewPetani/$1');
+$routes->get('petani', 'EthereumController::viewAllPetani');
